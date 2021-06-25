dbfa460b8bd7142560bb091f9dfb0af9cd314f2b
Bao <quocbaole5420@gmail.com>
2021-06-25 16:25:10 +0700

    data hết b null

--- src/containers/CourseDetail/index.js
+++ src/containers/CourseDetail/index.js
@@ -1,22 +1,50 @@
 import React from 'react'
-import { Axios } from 'axios'
 import { connect } from 'react-redux'
 import { useEffect } from 'react'
 import { actFetchCourseDetail } from './modules/action'
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
-function CourseDetailPage() {
-    // useEffect()
+function CourseDetailPage(props) {
+    useEffect(() => {
+        props.fetchCourseDetail()
+        console.log('course detail: ', props)
+    }, [])
     return (
-        <div>
-            this is CourseDetailPage
-        </div>
+        <Container >
+            <Box display="flex" >
+                <div>
+                    <h1>Mã hóa học: {props.data.maKhoaHoc}</h1>
+                    <p>Bí danh: {props.data.biDanh}</p>
+                    <p>Mô tả: {props.data.moTa}</p>
+                    <p>Số lượng học viên: {props.data.soLuongHocVien}</p>
+                    <p>Ngày tạo: {props.data.ngayTao}</p>
+                </div>
+                <img src={props.data.hinhAnh}></img>
+            </Box>
+        </Container>
     )
 }
 
 const mapStateToProps = (state) => {
     return {
         loading: state.courseDetailReducer.loading,
-        data: state.courseDetailReducer.data
+        data: state.courseDetailReducer.data || {
+            "maKhoaHoc": "",
+            "biDanh": "",
+            "tenKhoaHoc": "",
+            "moTa": "",
+            "luotXem": 0,
+            "hinhAnh": "",
+            "ngayTao": "",
+            "soLuongHocVien": 0,
+            "nguoiTao": {
+                "taiKhoan": "",
+                "hoTen": "",
+                "maLoaiNguoiDung": "",
+                "tenLoaiNguoiDung": ""
+            },
+        }
     }
 }
 
