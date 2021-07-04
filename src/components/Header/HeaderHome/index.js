f47f389468370debe643f9335ae4ccd0f6546ca7
vigeb <reach2101@gmail.com>
2021-07-04 20:32:19 +0700

    updated header home

--- src/components/Header/HeaderHome/index.js
+++ src/components/Header/HeaderHome/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { fade, makeStyles, styled } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
-// import { Link } from '@material-ui/core';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import FaceIcon from '@material-ui/icons/Face'
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
+  // grow: {
+  //   flexGrow: 1,
+  // },
+  toolBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -33,46 +31,46 @@ const useStyles = makeStyles((theme) => ({
       display: 'block',
     },
   },
-  search: {
-    position: 'relative',
+  // search: {
+  //   position: 'relative',
 
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-      display: 'block',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
+  //   borderRadius: theme.shape.borderRadius,
+  //   backgroundColor: fade(theme.palette.common.white, 0.15),
+  //   '&:hover': {
+  //     backgroundColor: fade(theme.palette.common.white, 0.25),
+  //   },
+  //   marginRight: theme.spacing(2),
+  //   marginLeft: 0,
+  //   width: '100%',
+  //   display: 'none',
+  //   [theme.breakpoints.up('sm')]: {
+  //     marginLeft: theme.spacing(3),
+  //     width: 'auto',
+  //     display: 'block',
+  //   },
+  // },
+  // searchIcon: {
+  //   padding: theme.spacing(0, 2),
+  //   height: '100%',
+  //   position: 'absolute',
+  //   pointerEvents: 'none',
+  //   display: 'flex',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // inputRoot: {
+  //   color: 'inherit',
+  // },
+  // inputInput: {
+  //   padding: theme.spacing(1, 1, 1, 0),
+  //   // vertical padding + font size from searchIcon
+  //   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+  //   transition: theme.transitions.create('width'),
+  //   width: '100%',
+  //   [theme.breakpoints.up('md')]: {
+  //     width: '20ch',
+  //   },
+  // },
   sectionDesktop: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
@@ -86,29 +84,11 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 
-
 }));
-
-const MyHeaderItem = styled(Button)({
-  boxShadow: 'none',
-  height: 60,
-})
 
 function Header(props) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
+  const [anchorEl, setAnchorEl] = React.useState(null)
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -117,37 +97,73 @@ function Header(props) {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
 
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+  const renderCredentials = () => {
+    if (props.credentials) {
+      return (
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <Typography style={{ padding: '6px', paddingLeft: '16px', fontSize: '1.5rem' }}>Xin chào,<br></br>{props.credentials.displayName} </Typography>
+          <MenuItem onClick={handleClose}>Hồ sơ</MenuItem>
+          <MenuItem onClick={handleClose}>Tài khoản của tôi</MenuItem>
+          <MenuItem onClick={handleClose}>Đăng xuất</MenuItem>
+        </Menu>
+      )
+    }
+    const localCredentials = localStorage.getItem("credentials") && JSON.parse(localStorage.getItem("credentials"))
 
-  const menuId = 'primary-search-account-menu';
+    if (localCredentials && localCredentials.displayName) {
+      return (
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <Typography style={{ padding: '6px', paddingLeft: '16px', fontSize: '1.5rem' }}>Xin chào,<br></br>{localCredentials.displayName} </Typography>
+          <MenuItem onClick={handleClose}>Hồ sơ</MenuItem>
+          <MenuItem onClick={handleClose}>Tài khoản của tôi</MenuItem>
+          <MenuItem onClick={handleClose}>Đăng xuất</MenuItem>
+        </Menu>
+      )
+    }
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-
-  console.log('5', props.credentials)
+    return (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <Link to='/signup'><MenuItem style={{ color: 'black' }}>
+          Đăng ký
+        </MenuItem></Link>
+        <Link to='/login'><MenuItem style={{ color: 'black' }}>
+          Đăng nhập
+        </MenuItem></Link>
+      </Menu>
+    )
+  }
+  
   return (
     <div className={classes.grow}>
       <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton> */}
+        <Toolbar className={classes.toolBar}>
           <Typography edge="start" className={classes.title} variant="h6" >
             <Link to="/">Trang chủ</Link>
           </Typography>
 
-          <div className={classes.search}>
+          {/* <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
@@ -159,91 +175,17 @@ function Header(props) {
               }}
               inputProps={{ 'aria-label': 'search' }}
             />
-          </div>
-          <div className={classes.grow} />
+          </div> */}
+          {/* <div className={classes.grow} /> */}
+
           <div className={classes.sectionDesktop}>
-            <>
-              {props.credentials ?
-                <>
-                  <MyHeaderItem variant="contained" color="primary">Xin chào {props.credentials.hoTen}</MyHeaderItem>
-                  <IconButton
-                    edge="end"
-                    aria-label="account of current user"
-                    aria-controls={menuId}
-                    aria-haspopup="true"
-                    onClick={handleProfileMenuOpen}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
-
-                </>
-                :
-
-                <>
-                  <Link to='/signup'><MyHeaderItem variant="contained" color="primary">
-                    Đăng ký
-                  </MyHeaderItem></Link>
-
-                  <Link to="/login">
-                    <MyHeaderItem variant="contained" color="primary">
-                      Đăng nhập
-                    </MyHeaderItem>
-                  </Link>
-                </>
-              }
-
-            </>
-
-          </div>
-
-          <div className={classes.sectionMobile}>
-
             <Button style={{ color: 'white' }} aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-              <MenuIcon />
+              <AccountCircleIcon />
             </Button>
-
-
+            {renderCredentials()}
           </div>
-
-
-
         </Toolbar>
       </AppBar>
-
-      {props.credentials ?
-        <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-          id={menuId}
-          keepMounted
-          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-          open={isMenuOpen}
-          onClose={handleMenuClose}
-        >
-          <Typography style={{ padding: '6px', paddingLeft: '16px', fontSize: '1.5rem' }}>Xin chào,<br></br>{props.credentials.hoTen} </Typography>
-          <MenuItem onClick={handleMenuClose}>Hồ sơ</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Tài khoản của tôi</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Đăng xuất</MenuItem>
-        </Menu>
-        : <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-          id={menuId}
-          keepMounted
-          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-          open={isMenuOpen}
-          onClose={handleMenuClose}
-        >
-          <Link to='/signup'><MenuItem style={{ color: 'black' }}>
-            Đăng ký
-          </MenuItem></Link>
-          <Link to='/login'><MenuItem style={{ color: 'black' }}>
-            Đăng nhập
-          </MenuItem></Link>
-        </Menu>
-      }
-
     </div >
   );
 }
@@ -251,8 +193,6 @@ function Header(props) {
 const mapStateToProps = (state) => {
   return {
     credentials: state.logInReducer.data,
-
-
   }
 }
 
