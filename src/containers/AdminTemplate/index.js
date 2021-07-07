45ce14118e448aac87f7b811d93bfcc91850db56
Bao <quocbaole5420@gmail.com>
2021-07-07 18:34:57 +0700

    fixed header

--- src/containers/AdminTemplate/index.js
+++ src/containers/AdminTemplate/index.js
@@ -18,6 +18,7 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { AdminListItem } from '../../components/ListItemAdmin';
 import { Route, Redirect } from 'react-router-dom'
+import Header from '../../components/Header';
 
 function Copyright() {
   return (
@@ -124,63 +125,44 @@ const AdminLayout = (props) => {
   };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Dashboard
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List><AdminListItem /></List>
-        <Divider />
-        {/* <List>{secondaryListItems}</List> */}
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          {props.children}
-          {/* <Grid container spacing={3}>
+    <>
+      <Header />
+      <div className={classes.root}>
+
+        <CssBaseline />
+
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List><AdminListItem /></List>
+          <Divider />
+          {/* <List>{secondaryListItems}</List> */}
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            {props.children}
+            {/* <Grid container spacing={3}>
             <Grid ite></Grid>
           </Grid> */}
-          <Box pt={4}>
-            <Copyright />
-          </Box>
-        </Container>
-      </main>
-    </div>
-    // <>
-      
-    //   {/* {props.children} */}
-    // </>
+            <Box pt={4}>
+              <Copyright />
+            </Box>
+          </Container>
+        </main>
+      </div>
+
+    </>
   )
 }
 
@@ -204,5 +186,5 @@ const AdminTemplate = ({ Component, ...props }) => {
     />
   );
 }
- 
+
 export default AdminTemplate;
